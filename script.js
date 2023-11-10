0432d8db1720cbb1d453d7e8fdd067f060d8a79b
sarahdp83 <144396487+sarahdp83@users.noreply.github.com>
2023-11-10 01:39:54 +0000

    all changes

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const changeProject = document.querySelector(".project");
+//const changeProject = document.querySelector(".project");
+
+const changeProject = document.querySelectorAll(".project");
 
 changeProject.addEventListener("click", function (event) {
   this.classList.add("clicked");
 });
+
+//changing only the first project - need to fix
+
+
